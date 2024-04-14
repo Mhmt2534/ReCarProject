eda2237ade534f352c6e3fe0b6e819c8cb8f3639
Mahmut Güneş <115086298+Mhmt2534@users.noreply.github.com>
2024-04-14 19:53:35 +0300

    Deneme

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -48,6 +48,7 @@ public static class Messages
     public static string MaintenanceTime = "SistemBakımda";
     public static string noRental = "Araba hala kullanımda";
     public static string Rentall = "Araba Kiralandı";
+
     internal static string ImageAdded;
     internal static string CarImageDeleted;
     internal static string ImageUpdated;
